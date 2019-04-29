e0467f0f4c972529e05efa7c5f28dd9d138c731a
HyukJin Kang <jinialadin1@naver.com>
2019-04-29 23:59:38 +0900

    Update 1107.cpp

--- 1107.cpp
+++ 1107.cpp
@@ -4,7 +4,7 @@
 #define MAX_INT 987654321
 
 int main(){
-    int Channel, num_broken, MadeChannel=0, digit=0;
+    int Channel, num_broken, MadeChannel1=0, MadeChannel2=0,MadeChannel=0, digit=0;
     bool broken[MAX_BROKEN];
     //initialize broken[]
     for(int i=0;i<MAX_BROKEN;++i)
@@ -25,6 +25,7 @@ int main(){
         
         //check the digit of input target channel
         for(int i=Channel;i>0;i/=10) digit++;
+        
         int temp_digit=digit;
         while(temp_digit--){
             int min=MAX_INT, min_digit=-1;
@@ -36,7 +37,7 @@ int main(){
             else{
                 for(int i=0;i<10;++i){
                     if(!broken[i]){
-                        int temp=(int) abs(Channel-i*pow(10,temp_digit)-MadeChannel);
+                        int temp=(int) abs(Channel-i*pow(10,temp_digit)-MadeChannel1);
                         if(min>temp){
                             min=temp;
                             min_digit=i;
@@ -45,8 +46,35 @@ int main(){
                 }
             }
             
-            MadeChannel+=min_digit*(int)pow(10,temp_digit);
+            MadeChannel1+=min_digit*(int)pow(10,temp_digit);
         }
+        
+        temp_digit=digit-1;
+        while(temp_digit--){
+            int min=MAX_INT, min_digit=-1;
+            int target_digit = (Channel/(int) pow(10,temp_digit))%10;
+            
+            if(!broken[target_digit]){
+                min_digit=target_digit;
+            }
+            else{
+                for(int i=0;i<10;++i){
+                    if(!broken[i]){
+                        int temp=(int) abs(Channel-i*pow(10,temp_digit)-MadeChannel2);
+                        if(min>temp){
+                            min=temp;
+                            min_digit=i;
+                        }
+                    }
+                }
+            }
+            
+            MadeChannel2+=min_digit*(int)pow(10,temp_digit);
+        }
+        
+        int bias1=abs(MadeChannel1-Channel);
+        int bias2=abs(MadeChannel2-Channel);
+        //MadeChannel=(abs(MadeChannel1-Channel)-abs(MadeChannel2-Channel))>0 ? MadeChannel2:MadeChannel1;
         std::cout<<std::min((int) abs(Channel-MadeChannel)+digit, (int) abs(Channel-100))<<'\n';
     }
     
